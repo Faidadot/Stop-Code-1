32231c051e4e440f26ec82f809f030fe4583f317
Federica <fed.vargasfrancisco@gmail.com>
2025-11-17 10:36:13 +0100

    Update EsercizioSei

--- StopAndCode1/src/EsercizioSei.java
+++ StopAndCode1/src/EsercizioSei.java
@@ -2,10 +2,10 @@ public class EsercizioSei {
     public static void main(String[] args) {
         final short annoNascita = 1998;
         short annoAttuale = 2025;
-        byte eta = (byte) ((byte) (annoAttuale + annoNascita) + 1);
+        byte eta = (byte) ((byte) (annoAttuale - annoNascita) + 1);
 
         System.out.println("Il numero dei tuoi anni +1 è un numero pari?");
-        String isPari = ((eta % 2 == 0) + 1) ? "Il numero è pari" : "Il numero è dispari";
+        String isPari = (eta % 2 == 0) ? "Il numero è pari" : "Il numero è dispari";
         System.out.println(isPari);
     }
 }
